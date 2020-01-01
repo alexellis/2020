7ce8058dab2aeeb9cf9f2335b23e0dd7e4fbf3ad
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-01-01 16:50:51 +0000

    Trim query
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- ascii-2020/handler.js
+++ ascii-2020/handler.js
@@ -2,7 +2,9 @@
 const querystring = require('querystring');
 
 module.exports = async (event, context) => {
-
+    if(event.query && event.query.indexOf("?")==0) {
+      event.query = event.query.substr(1)
+    }
     let qs = querystring.parse(event.query)
     if(!qs["q"]) {
       return context.status(400).fail("Give a message with ?q=msg-here")
