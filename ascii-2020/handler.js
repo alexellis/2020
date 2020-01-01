7fc46d49ce7df3a2fcb6d5c73d5a8be1e39a90be
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-01-01 17:00:42 +0000

    Return error when query not given
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- ascii-2020/handler.js
+++ ascii-2020/handler.js
@@ -3,16 +3,13 @@
 
 module.exports = async (event, context) => {
 
-//   if(event.query && event.query.indexOf("?")==0) {
-//      event.query = event.query.substr(1)
-//    }
-//    let qs = querystring.parse(event.query)
-//    if(!qs["q"]) {
-//      return context.status(400).fail("Give a message with ?q=msg-here")
-//    }
-    console.log(event)
+    if(!event.query["q"]) {
+      return context.headers({"Content-type": "text/plain"}).
+               status(400).
+               fail("Give a message with ?q=msg-here")
+    }
 
-    let msg = event.query["q"].toUpper()
+    let msg = event.query["q"].toUpperCase()
 
     let result = `┏━━┓┏━━┓┏━━┓┏━━┓
 ┗━┓┃┃┏┓┃┗━┓┃┃┏┓┃
