20036080a34e9a7dedd85ecc31ddabc5a10b2262
Alex Ellis <alexellis2@gmail.com>
2020-04-06 11:45:30 +0100

    Update handler.js

--- ascii-2020/handler.js
+++ ascii-2020/handler.js
@@ -3,7 +3,7 @@ const wordwrap = require('wordwrap')
 
 module.exports = async (event, context) => {
     if(!event.query['q']) {
-        return context.headers({'Content-type': 'text/plain'})
+        return context.headers({'Content-type': 'text/plain', "Access-Control-Allow-Origin": "http://localhost:8000"})
                 .status(400)
                 .fail('Give a message with ?q=msg-here')
     }
